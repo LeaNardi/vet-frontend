06e4c02b918d04b01a0ba6228823d0a2d5c19d0f
Leandro <leandroenardi@gmail.com>
2024-01-07 00:07:48 -0300

    Agregar mascota y conexion con backend

--- src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
+++ src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Mascota } from '../../interfaces/mascota';
+import { MascotaService } from '../../services/mascota.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -12,7 +14,9 @@ export class AgregarEditarMascotaComponent implements OnInit {
     loading: boolean = false;
     form: FormGroup
 
-    constructor(private fb: FormBuilder){
+    constructor(private fb: FormBuilder,
+        private _mascotaService: MascotaService,
+        private _snackBar: MatSnackBar){
         this.form = this.fb.group({
             nombre: ['',Validators.required],
             raza: ['',Validators.required],
@@ -34,5 +38,13 @@ export class AgregarEditarMascotaComponent implements OnInit {
             edad: this.form.get('edad')?.value,
             peso: this.form.get('peso')?.value,
         };
+        var message = "La mascota fue creada con exito";
+        var action = '';
+        var config = {duration: 4000};
+
+
+        this._mascotaService.addMascota(mascota).subscribe(data => {
+            this._snackBar.open(message, action, config);
+        })
     }
 }
